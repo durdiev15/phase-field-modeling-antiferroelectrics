54a67a01315c372a291092fb36d7b248c42ee376
durdiev15 <dilshod.durdiev15@gmail.com>
2025-07-30 13:47:24 +0200

    cleaned up

--- solvers/solver.py
+++ solvers/solver.py
@@ -97,7 +97,7 @@ class FourierSolver:
                        eps_ext: torch.Tensor        # (2,2,Nx,Ny) applied external strain (macro strain)
                        ):
         """
-        Solve the mechanical equilibrium equation, div sigma = 0, iteratively in Fourier space.
+        Solve the mechanical equilibrium equation, div sigma = 0, in Fourier space.
         
         Returns:
             sigma: Stress field (2,2,Nx,Ny)
@@ -123,58 +123,6 @@ class FourierSolver:
 
         return sigma, eps_elas
     
-    def solve_mechanics_pol(self, 
-                            Cx: torch.Tensor,             # (2,2,2,2,Nx,Ny) heterogeneous stiffness tensor
-                            C0: torch.Tensor,             # (2,2,2,2) homogeneous stiffness tensor
-                            G: torch.Tensor,              # (2,2,2,2,Nx,Ny) Green's operator
-                            eps0: torch.Tensor,           # (2,2,Nx,Ny) spontaneous strain
-                            eps_ext: torch.Tensor,         # (2,2,Nx,Ny) applied external strain (macro strain)
-                            number_interations = 100, tol = 1e-4):
-        """
-        Solve the mechanical equilibrium equation, div sigma = 0, iteratively in Fourier space.
-        
-        Returns:
-            sigma: Stress field (2,2,Nx,Ny)
-            eps_elas: Elastic strain field (2,2,Nx,Ny)
-            div_sigma: Divergence of stress field (2,Nx,Ny)
-        """
-        C0_complex = C0.to(dtype=self.dtype_fft)
-        G_complex = G.to(dtype=self.dtype_fft)
-        eps0_fft = fft(eps0, dim=(2,3))
-
-        sig_norm = 1e-8
-        eps_tot = torch.zeros_like(eps0, dtype=eps0.dtype, device=eps0.device)
-
-        for itr in range(number_interations):
-
-            eps_elas = eps_tot + eps_ext - eps0
-            sigma = torch.einsum('ijklx..., klx... -> ijx...', Cx, eps_elas)
-            sig_norm_new = torch.norm(sigma.ravel(), p=2).item()
-
-            err_s = abs((sig_norm_new - sig_norm) / sig_norm)
-            print(f"Iteration: {itr} | S. error = {err_s:.2E}")
-
-            if isnan(err_s) is True or isinf(err_s) is True:
-                print(f"Iteration loop terminated due to the presence of NaN or Inf ")
-                sys.exit(1)
-            elif err_s < tol:
-                break
-
-            sig_norm = sig_norm_new
-
-            tau = sigma - torch.einsum('ijkl, klx... -> ijx...', C0, eps_elas)
-            tau_fft = fft(tau, dim=(2,3))
-
-            alpha = tau_fft - torch.einsum('ijkl, klx... -> ijx...', C0_complex, eps0_fft)
-            eps_tot_fft = -torch.einsum('ijklx..., klx... -> ijx...', G_complex, alpha)
-
-            eps_tot = ifft(eps_tot_fft, dim=(2,3)).real
-        
-        del eps_tot, eps0_fft, tau
-        del tau_fft, alpha, eps_tot_fft, C0_complex, G_complex
-
-        return sigma, eps_elas
-    
     def solve_electrostatics(self,
                             K: float,              # permittivity
                             freq: torch.Tensor,    # (2, Nx, Ny) Fourier frequencies
@@ -182,7 +130,7 @@ class FourierSolver:
                             P_fft: torch.Tensor   # (2, Nx, Ny) polarization in Fourier space
                             ):
         """
-        Solve the electrostatic equation, divD = 0, iteratively in Fourier space.
+        Solve the electrostatic equation, divD = 0, in Fourier space.
         
         Returns:
             E: Electric field (2, Nx, Ny)
@@ -202,116 +150,3 @@ class FourierSolver:
         del freq2, E_fft
 
         return E, ifft(phi_fft).real
-
-    # def solve_mechanics(self, 
-    #                    C0: torch.Tensor,             # (2,2,2,2) stiffness tensor
-    #                    G: torch.Tensor,              # (2,2,2,2,Nx,Ny) Green's operator
-    #                    eps0: torch.Tensor,           # (2,2,Nx,Ny) spontaneous strain
-    #                    xi: torch.Tensor,             # (2,Nx,Ny) Fourier frequencies (kx, ky)
-    #                    eps_ext: torch.Tensor        # (2,2,Nx,Ny) applied external strain (macro strain)
-    #                    ):
-    #     """
-    #     Solve the mechanical equilibrium equation, div sigma = 0, iteratively in Fourier space.
-        
-    #     Returns:
-    #         sigma: Stress field (2,2,Nx,Ny)
-    #         eps_elas: Elastic strain field (2,2,Nx,Ny)
-    #         div_sigma: Divergence of stress field (2,Nx,Ny)
-    #     """
-
-    #     tau = -torch.einsum('ijkl,klxy->ijxy', C0, eps0)
-    #     tau_hat = fft(tau, dim=(-2, -1))
-    #     eps_hat = -torch.einsum('ijklmn,klmn->ijmn', G, tau_hat)
-
-    #     eps_ext_mean = fft(eps_ext, dim=(-2, -1))[:, :, 0, 0]
-    #     eps_hat[:, :, 0, 0] = eps_ext_mean # enforce mean strain
-    #     eps = ifft(eps_hat, dim=(-2, -1)).real
-
-    #     itr_total, res_data = [], []
-
-    #     alpha = 1.9
-    #     prev_res = float('inf')
-        
-    #     for itr in range(self.max_iter):
-    #         eps_elas = eps - eps0
-    #         sigma = torch.einsum('ijkl,klxy->ijxy', C0, eps_elas)
-    #         sigma_hat = fft(sigma, dim=(-2, -1))
-
-    #         div_sigma_hat = 1j * torch.einsum('ijxy,jxy->ixy', sigma_hat, xi)
-    #         div_sigma = ifft(div_sigma_hat, dim=(1, 2)).real
-
-    #         # res = torch.mean(div_sigma_hat**2).sqrt().item() / torch.mean(sigma_hat[:, :, 0, 0]**2).sqrt().item()
-    #         res = torch.mean(div_sigma ** 2).sqrt().item()
-
-    #         itr_total.append(itr)
-    #         res_data.append(res.real)
-
-    #         if res.real < self.tol:
-    #             break
-
-    #         # if res > prev_res:
-    #         #     alpha *= 0.8  # Slow down if oscillating
-    #         # else:
-    #         #     alpha = min(alpha * 1.15, 2.0)  # Careful not to overshoot
-    #         # prev_res = res
-            
-    #         eps_hat -= alpha * torch.einsum('ijklmn,klmn->ijmn', G, sigma_hat)
-    #         eps_hat[:, :, 0, 0] = eps_ext_mean
-    #         eps = ifft(eps_hat, dim=(2, 3)).real
-
-    #     del eps_hat, sigma_hat, div_sigma_hat, tau, xi, G
-
-    #     return sigma, eps_elas, div_sigma, itr_total, res_data
-    
-    # def solve_electrostatics(self,
-    #                         K: float,              # permittivity
-    #                         freq: torch.Tensor,    # (2, Nx, Ny) Fourier frequencies
-    #                         E_ext: torch.Tensor,   # (2, Nx, Ny) applied field
-    #                         P_fft: torch.Tensor   # (2, Nx, Ny) polarization in Fourier space
-    #                         ):
-    #     """
-    #     Solve the electrostatic equation, divD = 0, iteratively in Fourier space.
-        
-    #     Returns:
-    #         E: Electric field (2, Nx, Ny)
-    #         phi: Electric potential (Nx, Ny)
-    #         div_D: Divergence of electric displacement field (Nx, Ny)
-    #     """
-    #     freq2 = torch.einsum('i...,i...->...', freq, freq)
-    #     freq2[0, 0] = 1e-8
-
-    #     phi_hat = -1j * torch.einsum('i..., i...->...', freq, P_fft) / (K * freq2)
-
-    #     itr_total, res_data = [], []
-
-    #     alpha = 1.5
-    #     prev_res = float('inf')
-
-    #     for it in range(self.max_iter):
-    #         E_hat = -1j * freq * phi_hat 
-    #         E = ifft(E_hat, dim=(-2, -1)).real + E_ext
-
-    #         D = K * E + ifft(P_fft, dim=(-2, -1)).real
-    #         D_hat = fft(D, dim=(-2, -1))
-    #         div_D_hat = 1j * torch.einsum('ixy,ixy->xy', D_hat, freq)
-    #         div_D = ifft(div_D_hat).real
-
-    #         res = torch.mean(div_D ** 2).sqrt().item()
-            
-    #         itr_total.append(it)
-    #         res_data.append(res)
-
-    #         if res < self.tol:
-    #             break
-
-    #         # if res > prev_res:
-    #         #     alpha *= 0.8  # Slow down if oscillating
-    #         # else:
-    #         #     alpha = min(alpha * 1.05, 2.0)  # Careful not to overshoot
-    #         # prev_res = res
-
-    #         phi_hat -= alpha * div_D_hat / (K * freq2)
-
-    #     del freq2, div_D_hat
-
-    #     return E, ifft(phi_hat).real, div_D, D, itr_total, res_data
